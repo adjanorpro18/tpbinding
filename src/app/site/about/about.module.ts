615d912c868918183d38e18f12281bb6ec09e099
Enyon ADJANOR <enyonadjanor@gmail.com>
2021-07-26 12:02:31 +0200

    add routesChild

--- src/app/site/about/about.module.ts
+++ src/app/site/about/about.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path:'About', component:AboutComponent
+  }
+];
 
 @NgModule({
   declarations: [AboutComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AboutModule { }
